71d7ad5db61cc82b0e2147357d1483e4563316ab
kelvin Otuya <ifeanyiotuya@gmail.com>
2023-10-25 00:48:05 -0500

    updated k8s version to 1.25

--- variables.tf
+++ variables.tf
@@ -20,7 +20,7 @@ variable "cidr" {
 variable "cluster_version" {
   type        = string
   description = "The Kubernetes version for our clusters"
-  default     = "1.24"
+  default     = "1.25"
 }
 
 variable "cluster_instance_type" {
